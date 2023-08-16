f4f2512c7101f83015a71e7f14919656d28f791b
Casandra Normand <casandra.normand@outlook.com>
2023-08-15 23:38:46 -0400

    if counter <=0 font changes to red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,5 +5,11 @@ $(document).ready(function() {
     console.log($(this).val().length);
     let counter = $("#text-counter");
     counter.text(140 - $(this).val().length);
+    
+    if ($("#text-counter").val() <= 0){
+      $("#text-counter").css("color","#FF0000");
+    }else {
+      $("#text-counter").css("color", "#545149") 
+    };
   });
 });
