8d0fcc32236ad6c45a819e90b46f217088999c97
Casandra Normand <casandra.normand@outlook.com>
2023-08-24 01:08:47 -0400

    linting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,14 @@
 
 
 $(document).ready(function() {
-  $("#tweet-text-box").keyup(function(){
+  $("#tweet-text-box").keyup(function() {
     let counter = $("#text-counter");
     counter.text(140 - $(this).val().length);
     
-    if ($("#text-counter").val() <= 0){
+    if ($("#text-counter").val() <= 0) {
       $("#text-counter").css("color","#FF0000");
-    }else {
-      $("#text-counter").css("color", "#545149") 
-    };
+    } else {
+      $("#text-counter").css("color", "#545149")
+    }
   });
 });
