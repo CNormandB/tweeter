81b5faae8e2b75a6fd07b3d05fc767166bd12734
Casandra Normand <casandra.normand@outlook.com>
2023-08-24 00:33:20 -0400

    remove unnecessary console.log

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,6 @@
 
 $(document).ready(function() {
   $("#tweet-text-box").keyup(function(){
-    console.log($(this).val().length);
     let counter = $("#text-counter");
     counter.text(140 - $(this).val().length);
     
